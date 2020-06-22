4d2adf4ee6a42e755bb23201a900e627cec578ec
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-06-23 01:39:56 +0900

    split search code as page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
 import "./App.css";
-import SearchForm from "./components/SearchForm";
-import { useRecoilValueLoadable } from "recoil";
-import { bidListQuery } from "./lib/selectors";
-import "./App.css";
-import { SearchResultList } from "./components/SearchResultList";
+import SearchListPage from "./pages/SearchListPage";
 
 function App() {
-  const { state, contents } = useRecoilValueLoadable(bidListQuery);
   return (
     <div className="App">
-      <SearchForm loading={state === "loading"} />
-      <SearchResultList
-        loading={state === "loading"}
-        pageInfo={contents?.pageInfo}
-        itemList={contents?.bidItems}
-      />
+      <SearchListPage />
     </div>
   );
 }
