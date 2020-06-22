252b08028ec038c2917b58130ef82315a147750b
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-06-23 00:12:46 +0900

    query form with recoil

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import moment from "moment";
-import { Form, Input, DatePicker, Button } from "antd";
+import { Form, Input, DatePicker, Button, Spin } from "antd";
+import { QueryState } from "../lib/atoms";
+import { useRecoilState } from "recoil";
 import { defaultFromDate, defaultToDate } from "../lib/constants";
 const { RangePicker } = DatePicker;
 
@@ -13,43 +15,55 @@ const buttonItemLayout = {
   wrapperCol: { offset: 4, span: 16 },
 };
 
-const SearchForm = () => {
+const SearchForm = ({ loading = false }) => {
+  const [, setQuery] = useRecoilState(QueryState);
   const onFinish = (values) => {
-    console.log("Success:", values);
+    const { searchTerm, bidNum, bidDateRange } = values;
+    let [fromDate, toDate] = bidDateRange;
+    fromDate = fromDate.format("YYYY/MM/DD");
+    toDate = toDate.format("YYYY/MM/DD");
+    setQuery({
+      fromDate,
+      toDate,
+      searchTerm,
+      bidNum,
+    });
   };
 
   return (
-    <Form
-      onFinish={onFinish}
-      {...formItemLayout}
-      initialValues={{
-        bidDateRange: [
-          moment(defaultFromDate, "YYYY/MM/DD"),
-          moment(defaultToDate, "YYYY/MM/DD"),
-        ],
-      }}
-      layout="horizontal"
-      name="search-form"
-      size="large"
-      className="ant-search-form"
-    >
-      <div className="ant-search-form-wrapper">
-        <Form.Item name="bidNum" label="공고번호">
-          <Input placeholder={"공고번호를 입력하세요"} />
-        </Form.Item>
-        <Form.Item name="searchTerm" label="입찰건명">
-          <Input placeholder={"입찰건명을 입력하세요"} />
-        </Form.Item>
-        <Form.Item label="개찰마감일자 범위" name="bidDateRange">
-          <RangePicker style={{ width: "100%" }} format="YYYY/MM/DD" />
-        </Form.Item>
-        <Form.Item {...buttonItemLayout}>
-          <Button type="primary" htmlType="submit">
-            조회하기
-          </Button>
-        </Form.Item>
-      </div>
-    </Form>
+    <Spin spinning={loading}>
+      <Form
+        onFinish={onFinish}
+        {...formItemLayout}
+        initialValues={{
+          bidDateRange: [
+            moment(defaultFromDate, "YYYY/MM/DD"),
+            moment(defaultToDate, "YYYY/MM/DD"),
+          ],
+        }}
+        layout="horizontal"
+        name="search-form"
+        size="large"
+        className="ant-search-form"
+      >
+        <div className="ant-search-form-wrapper">
+          <Form.Item name="bidNum" label="공고번호">
+            <Input placeholder={"공고번호를 입력하세요"} />
+          </Form.Item>
+          <Form.Item name="searchTerm" label="입찰건명">
+            <Input placeholder={"입찰건명을 입력하세요"} />
+          </Form.Item>
+          <Form.Item label="개찰마감일자 범위" name="bidDateRange">
+            <RangePicker style={{ width: "100%" }} format="YYYY/MM/DD" />
+          </Form.Item>
+          <Form.Item {...buttonItemLayout}>
+            <Button type="primary" htmlType="submit">
+              조회하기
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </Spin>
   );
 };
 
